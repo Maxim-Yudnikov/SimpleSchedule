949cbfdd9b099a47a28bc85108412cff4fa77563
Diryfort <dorhungmi2@gmail.com>
2023-12-09 15:01:55 +0300

    3 xml

--- app/src/main/java/com/maxim/simpleschedule/main/presentation/MainActivity.kt
+++ app/src/main/java/com/maxim/simpleschedule/main/presentation/MainActivity.kt
@@ -2,11 +2,20 @@ package com.maxim.simpleschedule.main.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 import com.maxim.simpleschedule.R
+import com.maxim.simpleschedule.core.presentation.ProvideViewModel
+import com.maxim.simpleschedule.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ProvideViewModel {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun <T : ViewModel> viewModel(clasz: Class<out ViewModel>): T {
+        return (application as ProvideViewModel).viewModel(clasz)
     }
 }
