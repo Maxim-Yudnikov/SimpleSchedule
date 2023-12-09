ded570cf1ee93a8e003d7cd77f431b9cc799ce13
Diryfort <dorhungmi2@gmail.com>
2023-12-09 11:39:00 +0300

    ListInteractorTest

---
+++ app/src/test/java/com/maxim/simpleschedule/domain/ListInteractorTest.kt
@@ -0,0 +1,39 @@
+package com.maxim.simpleschedule.domain
+
+import com.maxim.simpleschedule.core.domain.DayDomain
+import com.maxim.simpleschedule.core.domain.LessonDomain
+import com.maxim.simpleschedule.list.domain.ListInteractor
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ListInteractorTest {
+
+    @Test
+    fun test_get_list() = runBlocking {
+        val dataSource = FakeListDataSource()
+        val interactor = ListInteractor.Base(dataSource)
+
+        dataSource.returnEmpty = true
+        var actual = interactor.getList()
+        assertEquals(listOf(DayDomain.Base(123,"start", "end", listOf(LessonDomain.Empty))), actual)
+
+        dataSource.returnEmpty = true
+        actual = interactor.getList()
+        assertEquals(listOf(DayDomain.Base(123,"start", "end", listOf(LessonDomain.Base("name")))), actual)
+    }
+
+    private class FakeListDataSource : ListDataSource {
+        var returnEmpty = true
+        override suspend fun getList(): List<DayDomain> {
+            return listOf(
+                DayDomain.Base(
+                    123,
+                    "start",
+                    "end",
+                    if (returnEmpty) emptyList() else listOf(LessonDomain.Base("name"))
+                )
+            )
+        }
+    }
+}
